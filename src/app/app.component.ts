609d400ee4daf515eafb08cae9516d16817a22f1
Krishnam Raju Revu <krishnamrajurevu@gmail.com>
2025-05-08 21:16:41 +0530

    form task added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { TaskFormComponent } from './components/task-form/task-form.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [TaskFormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
